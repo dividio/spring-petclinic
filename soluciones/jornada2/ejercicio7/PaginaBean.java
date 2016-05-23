9f6c5e347a4cc2068704cc4083d95228dfe58598
dividio <dividio@gmail.com>
2016-05-23 11:11:07 +0200

    - Añadidos ejercicios 5, 6, y 7 de la jornada 2.

---
+++ soluciones/jornada2/ejercicio7/PaginaBean.java
@@ -0,0 +1,18 @@
+package cursojsf.jornadas.jornada2;
+
+public class PaginaBean {
+
+	private String nombrePagina;
+
+	public String getNombrePagina() {
+		return nombrePagina;
+	}
+
+	public void setNombrePagina(String nombrePagina) {
+		this.nombrePagina = nombrePagina;
+	}
+
+	public String volver() {
+		return "volver";
+	}
+}
