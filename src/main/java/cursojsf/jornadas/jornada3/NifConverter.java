98f0ba81382e8b2d000368484b29236bf7bc3392
jjmena <jacintomena80@gmail.com>
2016-05-24 09:39:04 +0200

    Se describen los ejercicio existentes de la jornada 3

--- src/main/java/cursojsf/jornadas/jornada3/NifConverter.java
+++ src/main/java/cursojsf/jornadas/jornada3/NifConverter.java
@@ -13,8 +13,19 @@ public class NifConverter implements Converter {
 	@Override
 	public Object getAsObject(FacesContext arg0, UIComponent arg1, String arg2) {
 		if (arg2 != null) {
-			DecimalFormat df = new DecimalFormat("00000009");
-			return df.format(Integer.valueOf(arg2));
+			DecimalFormat df = new DecimalFormat("00000000");
+			try {
+				if (arg2.length() > 1) {
+					// Sacamos el último carácter
+					String letra = arg2.substring(arg2.length() - 1, arg2.length());
+					String numero = arg2.substring(0, arg2.length() - 1);
+					return df.format(Integer.valueOf(numero)) + letra.toUpperCase();
+				} else {
+					return arg2;
+				}
+			} catch (Exception e) {
+				return arg2;
+			}
 		}
 		// TODO Auto-generated method stub
 		return null;
@@ -27,6 +38,5 @@ public class NifConverter implements Converter {
 		}
 		return arg2.toString();
 	}
-
 
 }
