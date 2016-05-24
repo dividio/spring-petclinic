5255c49f4a59c6a33f07b733713e96d3a7a4fb5e
jjmena <jacintomena80@gmail.com>
2016-05-24 13:31:39 +0200

    Se sube la práctica final de la jornada 1

--- src/main/java/cursojsf/practica/DetalleMascotaBean.java
+++ src/main/java/cursojsf/practica/DetalleMascotaBean.java
@@ -1,5 +1,98 @@
 package cursojsf.practica;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.model.SelectItem;
+
+import org.joda.time.LocalDate;
+import org.springframework.samples.petclinic.model.Owner;
+import org.springframework.samples.petclinic.model.Pet;
+import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.samples.petclinic.service.ClinicService;
+
+@ManagedBean(name = "detalleMascotaBean")
 public class DetalleMascotaBean {
+
+	private Pet mascota;
+
+	private List<PetType> tiposMascota;
+
+	private Boolean modoConsulta;
+
+	public Pet getMascota() {
+		return mascota;
+	}
+
+	public void setMascota(Pet mascota) {
+		this.mascota = mascota;
+	}
+
+	public List<PetType> getTiposMascota() {
+		return tiposMascota;
+	}
+
+	public void setTiposMascota(List<PetType> tiposMascota) {
+		this.tiposMascota = tiposMascota;
+	}
+
+	public Boolean getModoConsulta() {
+		return modoConsulta;
+	}
+
+	public void setModoConsulta(Boolean modoConsulta) {
+		this.modoConsulta = modoConsulta;
+	}
+
+	public ClinicService getClinicService() {
+		return clinicService;
+	}
+
+	public void setClinicService(ClinicService clinicService) {
+		this.clinicService = clinicService;
+	}
+
+	@ManagedProperty(value = "#{clinicServiceImpl}")
+	private ClinicService clinicService;
+
+	public DetalleMascotaBean() {
+		this.mascota = getPetDummy();
+	}
+	
+	public  List<PetType> getPetTypes() {
+		tiposMascota = new ArrayList<>(clinicService.findPetTypes());
+		return tiposMascota;
+	}
+
+	private Pet getPetDummy() {
+		
+		Pet resultado = new Pet();
+		Owner owner = getOwnerDummy(0);
+
+		resultado.setName("Mascota ");
+		resultado.setBirthDate(LocalDate.fromCalendarFields(Calendar.getInstance()));
+		resultado.setId(1);
+		owner.addPet(resultado);
+		
+		return resultado;
+	}
+	
+	private Owner getOwnerDummy(int index) {
+		Owner resultado = new Owner();
+
+		resultado.setAddress("Dirección " + index);
+		resultado.setCity("Ciudad " + index);
+		resultado.setFirstName("Nombre " + index);
+		resultado.setId(index);
+		resultado.setLastName("Apellido " + index);
+		resultado.setTelephone("+" + index + " 999999999");
+		
+		return resultado;
+	}
+
+	
 
 }
--- src/main/java/cursojsf/practica/PropietariosBean.java
+++ src/main/java/cursojsf/practica/PropietariosBean.java
@@ -1,5 +1,66 @@
 package cursojsf.practica;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+import org.springframework.samples.petclinic.model.Owner;
+import org.springframework.samples.petclinic.model.Pet;
+
+@ManagedBean(name = "propietariosBean")
 public class PropietariosBean {
+
+	private List<Owner> propietarios;
+
+	private String filtro;
+
+	public List<Owner> getPropietarios() {
+		return propietarios;
+	}
+
+	public void setPropietarios(List<Owner> propietarios) {
+		this.propietarios = propietarios;
+	}
+
+	public String getFiltro() {
+		return filtro;
+	}
+
+	public void setFiltro(String filtro) {
+		this.filtro = filtro;
+	}
+
+	public PropietariosBean() {
+		this.propietarios = this.getDummy();
+	}
+
+	private List<Owner> getDummy() {
+		List<Owner> resultado = new ArrayList<>();
+
+		for (int i = 0; i < 10; i++) {
+			resultado.add(getOwnerDummy(i));
+		}
+
+		return resultado;
+	}
+
+	private Owner getOwnerDummy(int index) {
+		Owner resultado = new Owner();
+
+		resultado.setAddress("Dirección " + index);
+		resultado.setCity("Ciudad " + index);
+		resultado.setFirstName("Nombre " + index);
+		resultado.setId(index);
+		resultado.setLastName("Apellido " + index);
+		resultado.setTelephone("+" + index + " 999999999");
+		
+		// Se añade una mascota
+		Pet pet = new Pet();
+		pet.setName("Mascota " + index);
+		resultado.addPet(pet);
+
+		return resultado;
+	}
 
 }
