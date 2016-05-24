8ced3cd5a386f04719ec939c1831d90d32a801d6
dividio <dividio@gmail.com>
2016-05-24 13:54:36 +0200

    Añadida detalle del propietario de la practica 1

--- src/main/java/cursojsf/practica/DetallePropietarioBean.java
+++ src/main/java/cursojsf/practica/DetallePropietarioBean.java
@@ -1,5 +1,72 @@
 package cursojsf.practica;
 
-public class DetallePropietarioBean {
+import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+import org.springframework.samples.petclinic.model.Owner;
+import org.springframework.samples.petclinic.model.Pet;
+import org.springframework.samples.petclinic.service.ClinicService;
+
+@ManagedBean(name = "detallePropietarioBean")
+public class DetallePropietarioBean {
+	
+    @ManagedProperty(value = "#{clinicServiceImpl}")
+    private ClinicService clinicService;
+    
+    private Owner propietario;
+    
+    private List<Pet> listaMascotas;
+    
+    private Boolean modoConsulta = Boolean.FALSE;
+    
+    public ClinicService getClinicService() {
+		return clinicService;
+	}
+
+	public void setClinicService(ClinicService clinicService) {
+		this.clinicService = clinicService;
+	}
+
+	public Owner getPropietario() {
+		return propietario;
+	}
+
+	public void setPropietario(Owner propietario) {
+		this.propietario = propietario;
+	}
+
+	public List<Pet> getListaMascotas() {
+		return listaMascotas;
+	}
+
+	public void setListaMascotas(List<Pet> listaMascotas) {
+		this.listaMascotas = listaMascotas;
+	}
+
+	public Boolean getModoConsulta() {
+		return modoConsulta;
+	}
+
+	public void setModoConsulta(Boolean modoConsulta) {
+		this.modoConsulta = modoConsulta;
+	}
+
+	public DetallePropietarioBean() {
+		this.propietario = getOwnerDummy(1);
+	}
+	
+	private Owner getOwnerDummy(int index) {
+		Owner resultado = new Owner();
+
+		resultado.setAddress("Dirección " + index);
+		resultado.setCity("Ciudad " + index);
+		resultado.setFirstName("Nombre " + index);
+		resultado.setId(index);
+		resultado.setLastName("Apellido " + index);
+		resultado.setTelephone("+" + index + " 999999999");
+		
+		return resultado;
+	}
 }
